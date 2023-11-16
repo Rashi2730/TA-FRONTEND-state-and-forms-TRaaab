da0ff5e031916c29e2190257e56cd5162f904008
Rashi Israni <rashiisrani@Rashis-MacBook-Air.local>
2023-11-16 16:35:47 +0530

    Accordian app

--- block-BRaabi/faq/src/App.js
+++ block-BRaabi/faq/src/App.js
@@ -1,12 +1,37 @@
 import React from 'react';
 import Header from './Header';
+import faqs from './ques.json';
 
-function App() {
-  return (
-    <>
-      <Header />
-    </>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeQues: null,
+    };
+  }
+  render() {
+    return (
+      <>
+        <Header />
+        <ul>
+          {faqs.map((ques, i) => (
+            <li
+              className={this.state.activeQues === i ? 'active--ques' : 'ques'}
+            >
+              <h2
+                onClick={() => {
+                  this.setState({ activeQues: i });
+                }}
+              >
+                {ques.Q} {this.state.activeQues === i ? '👆🏻' : '👇🏻'}
+              </h2>
+              {i === this.state.activeQues && <p>{ques.A}</p>}
+            </li>
+          ))}
+        </ul>
+      </>
+    );
+  }
 }
 
 export default App;
